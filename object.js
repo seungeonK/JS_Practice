e011636b0aa1b8dfda1c5359c23515e50e84d9eb
hjk6281 <hjk6281@gmail.com>
2021-01-22 01:00:25 -0500

    added object practice and conditional operator practice

--- object.js
+++ object.js
@@ -1,6 +1,25 @@
-const object1 = {
-  name:"Seungeon"
-  age:22
-  origin:"South Korea"
-  gender:"male"
+// object is something like an array.
+// but it is a bit different
+// object can contain different data types
+// object can even contain arrays, or another object inside
+
+const seungeon = {
+  name:"Seungeon",
+  age:22,
+  origin:"South Korea",
+  gender:"male",
+  isHandsome:false,
+  favMovie:["Harry Potter", "Parasite"],
+  family:{
+    sibling:1,
+    mom:"mom",
+    dad:"dad",
+    numOfFamily:4
   }
+}
+
+console.log(seungeon,'\n');
+
+seungeon.gender = "Female"; // object instance variable can be changed
+
+console.log(seungeon.gender);
---
+++ ternary operator.js
@@ -0,0 +1,12 @@
+// ternary operator '?'
+// usage -> let result = condition ? value1 : value2;
+
+let tobaccoAge = (age >= 22) ? true : false;
+
+if (age >= 22) {
+    tobaccoAge = true;
+} else {
+    tobaccoAge = false;
+}
+
+//they both same
